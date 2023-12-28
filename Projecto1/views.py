729fa159c8ceba602b43fa7d899249ae571c1a51
German Gonzalez <ggz990@gmail.com>
2023-12-27 21:31:32 -0300

    Tercer Commit

--- Projecto1/views.py
+++ Projecto1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 
 def saludo(request):
     return HttpResponse('Primera Vista')
@@ -7,14 +8,14 @@ def segunda_vista(request):
     return HttpResponse('Trabajo Preliminar')
 
 def Template(request):
-    
+
     mihtml = open(r'C:\Users\ggz99\Python\Django2\Tercera_Preentrega\Projecto1\templates\template1.html')
 
     plantilla = Template(mihtml.read())
     
     mihtml.close()
 
-    miContexto = Context()
+    miContexto =  Context()
     
     documento = plantilla.render(miContexto)
 
